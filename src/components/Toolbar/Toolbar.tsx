ce2f7168829fe802b0608b7eae5745e028e70347
Khrunev Denis <deniskhrunev@gmail.com>
2024-06-24 13:36:33 +0600

    Создан компонент Toolbar отвечающий за отрисовку навигации и перехода по странице

---
+++ src/components/Toolbar/Toolbar.tsx
@@ -0,0 +1,26 @@
+import {NavLink} from 'react-router-dom';
+
+const Toolbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+      <div className="container-fluid container">
+        <NavLink to='/' className='navbar-brand'>My profile</NavLink>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink to='/' className='nav-link'>About us</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to='/portfolio' className='nav-link'>Portfolio</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to='/contacts' className='nav-link'>Contacts</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Toolbar;
